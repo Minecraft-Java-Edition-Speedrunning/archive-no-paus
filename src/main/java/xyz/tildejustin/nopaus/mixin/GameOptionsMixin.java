bbc8cab79b8dded6cdcfdf731ae73b1429f303b2
tildejustin <spacepepper3.14@gmail.com>
2023-04-05 22:26:27 -0400

    no more annoying error

--- src/main/java/xyz/tildejustin/nopaus/mixin/GameOptionsMixin.java
+++ src/main/java/xyz/tildejustin/nopaus/mixin/GameOptionsMixin.java
@@ -4,33 +4,25 @@ import net.minecraft.client.option.GameOptions;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import xyz.tildejustin.nopaus.NoPaus;
 
-import java.io.BufferedReader;
-import java.io.File;
 import java.io.PrintWriter;
 
 @Mixin(GameOptions.class)
 public class GameOptionsMixin {
-	@Inject(at = @At(value = "INVOKE", target = "Ljava/io/PrintWriter;println(Ljava/lang/String;)V", ordinal = 29, shift = At.Shift.AFTER), method = "save", locals = LocalCapture.CAPTURE_FAILHARD)
-	private void savePauseOnLostFocus(CallbackInfo ci, PrintWriter printWriter) {
-		printWriter.println("pauseOnLostFocus:" + NoPaus.pauseOnLostFocus);
-	}
+    @Inject(at = @At(value = "INVOKE", target = "Ljava/io/PrintWriter;println(Ljava/lang/String;)V", ordinal = 29, shift = At.Shift.AFTER), method = "save", locals = LocalCapture.CAPTURE_FAILHARD)
+    private void savePauseOnLostFocus(CallbackInfo ci, PrintWriter printWriter) {
+        printWriter.println("pauseOnLostFocus:" + NoPaus.pauseOnLostFocus);
+    }
 
-	@Inject(at = @At(value = "INVOKE", target = "Ljava/lang/String;split(Ljava/lang/String;)[Ljava/lang/String;", shift = At.Shift.BY, by = 2), method = "load", locals = LocalCapture.CAPTURE_FAILSOFT)
-	private void loadPauseOnLostFocus(CallbackInfo ci, BufferedReader bufferedReader, String string, String[] stringArray) {
-		if (stringArray[0].equals("pauseOnLostFocus")) {
-			NoPaus.pauseOnLostFocus = stringArray[1].equals("true");
-		}
-	}
-
-	// optifine support
-	@Inject(at = @At(value = "INVOKE", target = "Ljava/lang/String;split(Ljava/lang/String;)[Ljava/lang/String;", shift = At.Shift.BY, by = 2), method = "load", locals = LocalCapture.CAPTURE_FAILSOFT)
-	private void loadPauseOnLostFocus(CallbackInfo ci, File file, BufferedReader bufferedReader, String string, String[] stringArray) {
-		if (stringArray[0].equals("pauseOnLostFocus")) {
-			NoPaus.pauseOnLostFocus = stringArray[1].equals("true");
-		}
-	}
+    @ModifyVariable(method = "load", at = @At(value = "STORE", ordinal = 0))
+    private String[] loadPauseOnLostFocus(String[] stringArray) {
+        if (stringArray[0].equals("pauseOnLostFocus")) {
+            NoPaus.pauseOnLostFocus = stringArray[1].equals("true");
+        }
+        return stringArray;
+    }
 }
